e36d16c73af23540f45c81b9932a5b091d1ecece
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2014-01-25 02:48:01 +0400

    Add git aliases.

--- config.fish
+++ config.fish
@@ -9,15 +9,20 @@ set -x EDITOR "stty stop '' -ixoff ; vim"
 # Aliases
 alias e "$EDITOR"
 alias x 'extract'
+alias g 'git'
+alias ga 'git add'
+alias gaa 'git add .'
+alias gc 'git commit'
+alias gp 'git push'
+alias gl 'git pull'
 
 # Plugins
-function Plugin -d 'Enable plugin';
-    set -g fish_plugins $argv $fish_plugins;
+function Plugin -d 'Enable plugin'
+    set -g fish_plugins $argv $fish_plugins
 end
 Plugin 'extract'
 Plugin 'gitignore'
 Plugin 'jump'
-Plugin 'git'
 
 set fish_custom $HOME/.config/fish
 
